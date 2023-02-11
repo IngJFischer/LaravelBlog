6289766f026595f2750f0cf6b83ef2545894219d
Jeremy <ing_fischer@outlook.com>
2023-02-11 15:03:15 -0300

    Laravel Sanctum API Authentication

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Auth\LoginRequest;
+
+class UserController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (Auth::attempt($credentials)){
+            $token = Auth::user()->createToken('myapptoken')->plainTextToken;
+            return response()->json($token);
+        };
+        return response()->json('Usuario o Contraseña Inválido');
+    }
+
+    public function logout(LoginRequest $request)
+    {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (Auth::attempt($credentials)){
+            Auth::user()->tokens()->delete();
+            return response()->json('Loged Out');
+        };
+        return response()->json('Usuario o Contraseña Inválido');
+    }
+}
